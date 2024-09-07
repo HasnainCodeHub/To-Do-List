c85d4fbe09f504ad9729076f7585810e342dcd84
Hasnain-Software-Developer <163559600+Hasnain-Software-Developer@users.noreply.github.com>
2024-09-08 03:09:56 +0500

    code

---
+++ main.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+console.log("\n\t Welcome to Hasnain's Coding World\n");
+import inquirer from "inquirer";
+let todos = [];
+let condition = (true);
+while (condition) {
+    let answer = await inquirer.prompt([{
+            message: "What do you want to Add in Your Todos ?",
+            type: "input",
+            name: "todo"
+        },
+        {
+            message: "Do you want To Add More ?",
+            type: "confirm",
+            name: "Opinion",
+            default: "false"
+        }
+    ]);
+    todos.push(answer.todo);
+    condition = answer.Opinion;
+    console.log(todos);
+}
